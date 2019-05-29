fcb22eff62b0298c78105f1acd47c3fbbbd2b0c5
Giulliano Bueno <giubueno@gmail.com>
2019-05-29 21:10:50 +0200

    Add empty TopBar component

---
+++ app/components/TobBar/index.js
@@ -0,0 +1,24 @@
+/**
+ *
+ * TobBar
+ *
+ */
+
+import React, { memo } from 'react';
+// import PropTypes from 'prop-types';
+// import styled from 'styled-components';
+
+import { FormattedMessage } from 'react-intl';
+import messages from './messages';
+
+function TobBar() {
+  return (
+    <div>
+      <FormattedMessage {...messages.header} />
+    </div>
+  );
+}
+
+TobBar.propTypes = {};
+
+export default memo(TobBar);
---
+++ app/components/TobBar/messages.js
@@ -0,0 +1,16 @@
+/*
+ * TobBar Messages
+ *
+ * This contains all the text for the TobBar component.
+ */
+
+import { defineMessages } from 'react-intl';
+
+export const scope = 'app.components.TobBar';
+
+export default defineMessages({
+  header: {
+    id: `${scope}.header`,
+    defaultMessage: 'This is the TobBar component!',
+  },
+});
