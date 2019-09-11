18b5784a5e164959fe506b5c31ef48ba1e60a4d5
Giulliano Bueno <giubueno@gmail.com>
2019-09-11 18:33:12 +0200

    Redirect home to /events

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -6,13 +6,8 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import { Redirect } from 'react-router-dom';
 
 export default function HomePage() {
-  return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
-  );
+  return <Redirect to="/events" />;
 }
